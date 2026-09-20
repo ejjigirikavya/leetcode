496d797f5a5f25e5eba59917a2cb4e903aa557c4
ejjigirikavya <mailtokavyaejjigiri@gmail.com>
2026-09-20 18:28:42 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (67.44%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum=0;
+       for(int i=0;i<s.length();i++){
+        int normal=s.charAt(i)-'a'+1;
+        int value=27-normal;
+        sum+=(i+1)*value;
+       } 
+       return sum;
+    }
+}
