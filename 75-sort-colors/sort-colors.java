c06708288cf26586776bba108fef99ce65cd403e
ejjigirikavya <mailtokavyaejjigiri@gmail.com>
2026-09-22 18:27:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (75.81%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int i=0;
+        int j=0;
+        int k=nums.length-1;
+        int temp=0;
+        while(j<=k){
+            if(nums[j]==0){
+                temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+                j++;
+            }
+            else if(nums[j]==1){
+                j++;
+            }
+            else{
+                temp=nums[j];
+                nums[j]=nums[k];
+                nums[k]=temp;
+                k--;
+            }
+        }
+       
+    }
+}
